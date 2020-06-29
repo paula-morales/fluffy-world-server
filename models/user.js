1ecec682c73cbee56da09118d4270c8f5fbb670d
paula-morales <pmpoclava@gmail.com>
2020-06-29 23:38:36 +0200

    models user/userService/service

--- models/user.js
+++ models/user.js
@@ -3,24 +3,53 @@ module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define(
     "user",
     {
-      name: {
+      firstName: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phoneNumber: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      profilePicture: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
       email: {
         type: DataTypes.STRING,
         unique: true,
-        allowNull: false
+        allowNull: false,
       },
       password: {
         type: DataTypes.STRING,
-        allowNull: false
-      }
+        allowNull: false,
+      },
+      latitude: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      longitude: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+
+      isOwner: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      isCandidate: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
     },
     {}
   );
-  user.associate = function(models) {
-    // associations can be defined here
+  user.associate = function (models) {
+    user.hasMany(models.userservice);
   };
   return user;
 };
