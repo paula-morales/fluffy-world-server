a0bd3520cb89a3cf81c844ea2513501e668b1399
paula-morales <pmpoclava@gmail.com>
2020-07-03 23:27:05 +0200

    relations favorites

---
+++ migrations/20200703205508-create-favorite.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("favorites", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      userServiceId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "userServices",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("favorites");
+  },
+};
