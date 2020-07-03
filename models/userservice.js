ec194bdbe792ad413667ba7831c98d6b393ab120
paula-morales <pmpoclava@gmail.com>
2020-07-03 20:30:11 +0200

    new columns models&seeds

--- models/userservice.js
+++ models/userservice.js
@@ -20,6 +20,9 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.TEXT,
         allowNull: false,
       },
+      availableFrom: DataTypes.STRING,
+
+      availableUntil: DataTypes.STRING,
     },
     {}
   );
