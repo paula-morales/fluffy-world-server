eaade5b712d6ab6444b796607b72e5f1e121634b
paula-morales <pmpoclava@gmail.com>
2020-07-04 01:14:21 +0200

    router /user/favorites

--- routers/user.js
+++ routers/user.js
@@ -5,6 +5,23 @@ const User = require("../models").user;
 const Favorites = require("../models").favorite;
 const authMiddleware = require("../auth/middleware");
 
+router.get("/favorites", authMiddleware, async (req, res, next) => {
+  const userLogged = req.user.dataValues;
+  // console.log("userid", userLogged.id);
+
+  try {
+    const getFavorites = await Favorites.findAll({
+      where: { userId: userLogged.id },
+    });
+
+    res.status(201).json(getFavorites);
+  } catch (e) {
+    return res
+      .status(400)
+      .send({ message: " here Something went wrong, sorry" });
+  }
+});
+
 router.get("/:userId", async (req, res, next) => {
   const { userId } = req.params;
   parseInt(userId);
@@ -16,17 +33,4 @@ router.get("/:userId", async (req, res, next) => {
   }
 });
 
-router.get("/favorites", async (req, res, next) => {
-  // console.log(req.headers);
-  // const userLogged = req.user.dataValues;
-  // console.log("userid", userLogged.id);
-  console.log("HEREEEEEE");
-  try {
-    const getFavorites = await Favorites.findAll();
-    console.log("fav", getFavorites.dataValues);
-    res.status(201).json(getFavorites);
-  } catch (e) {
-    return res.status(400).send({ message: "Something went wrong, sorry" });
-  }
-});
 module.exports = router;
